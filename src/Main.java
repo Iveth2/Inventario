1b1fdeeae5a07aca5345cb037790ec944fac284a
Angel Geovanni Marcial Morales <Skatepanuco@gmail.com>
2024-02-21 22:04:32 -0600

    q

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,8 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("hi");
+        System.out.println("iveth");
+        System.out.println("Angel");
     }
     
 }
